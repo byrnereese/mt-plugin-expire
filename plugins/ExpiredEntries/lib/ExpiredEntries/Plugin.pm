277d9bc08062cd2cc16d71dff858dc1cac74e795
Byrne <byrne@majordojo.com>
2010-11-24 10:08:55 -0800

    tidying

--- plugins/ExpiredEntries/lib/ExpiredEntries/Plugin.pm
+++ plugins/ExpiredEntries/lib/ExpiredEntries/Plugin.pm
@@ -7,8 +7,8 @@ use strict;
 use warnings;
 
 use Carp qw( croak longmess confess );
-use MT::Util qw( relative_date    offset_time format_ts days_in 
-                 offset_time_list epoch2ts    ts2epoch  trim );
+use MT::Util qw( relative_date    offset_time format_ts days_in
+  offset_time_list epoch2ts    ts2epoch  trim );
 
 sub MT::Entry::EXPIRED () { 6 }
 
@@ -19,78 +19,85 @@ sub _entry_label {
 }
 
 sub hdlr_expire_date {
-    my ($ctx, $args) = @_;
+    my ( $ctx, $args ) = @_;
     my $e = $ctx->stash('entry')
-        or return $ctx->_no_entry_error();
+      or return $ctx->_no_entry_error();
     return '' unless $e->expire_on;
     $args->{ts} = $e->expire_on;
     require MT::Template::ContextHandlers;
-    return MT::Template::Context::_hdlr_date($ctx, $args);
+    return MT::Template::Context::_hdlr_date( $ctx, $args );
 }
 
 sub load_tasks {
     my $cfg = MT->config;
     return {
-	'ExpirePost' => {
-	  'label' => 'Unpublish Expired Entries',
-	  'frequency' => $cfg->ExpirePostFrequency * 60,
-	  'code' => sub { 
-	      ExpiredEntries::Plugin->task_expire; 
-	  },
-      
-    }
+        'ExpirePost' => {
+            'label'     => 'Unpublish Expired Entries',
+            'frequency' => $cfg->ExpirePostFrequency * 60,
+            'code'      => sub {
+                ExpiredEntries::Plugin->task_expire;
+            },
+
+        }
     };
 }
 
 sub frequency {
-    my $mt = MT->instance;
-    my $cfg = $mt->{config};
+    my $mt   = MT->instance;
+    my $cfg  = $mt->{config};
     my $freq = $cfg->ExpirePostFrequency || 1;
     return $freq * 60;
 }
 
 sub xfrm_edit_param {
-    my ($cb, $app, $param, $tmpl) = @_;
+    my ( $cb, $app, $param, $tmpl ) = @_;
     my $entry_id = $app->param('id') or return;
-    my $blog = $app->blog;
-    my $obj = MT->model('entry')->load( $entry_id )
-        or return $cb->error('failed to load entry');
+    my $blog     = $app->blog;
+    my $obj      = MT->model('entry')->load($entry_id)
+      or return $cb->error('failed to load entry');
 
-    $param->{ "status_expired" } = 1 if ($obj->status == MT::Entry::EXPIRED());
+    $param->{"status_expired"} = 1 if ( $obj->status == MT::Entry::EXPIRED() );
 
     $param->{'expire_on_date'} = $app->{query}->param('expire_on_date')
-	|| format_ts( "%Y-%m-%d", $obj->expire_on, $blog, $app->user ? $app->user->preferred_language : undef );
+      || format_ts( "%Y-%m-%d", $obj->expire_on, $blog,
+        $app->user ? $app->user->preferred_language : undef );
     $param->{'expire_on_time'} = $app->{query}->param('expire_on_time')
-	|| format_ts( "%H:%M:%S", $obj->expire_on, $blog, $app->user ? $app->user->preferred_language : undef );    
+      || format_ts( "%H:%M:%S", $obj->expire_on, $blog,
+        $app->user ? $app->user->preferred_language : undef );
 
-    if ($param->{'expire_on_date'} eq '' || $param->{'expire_on_date'} eq '0000-00-00') {
-	$param->{'expire_on_date'} = '';
-	$param->{'expire_on_time'} = '';
-    } else {
-	$param->{'has_expire'} = 1;
+    if (   $param->{'expire_on_date'} eq ''
+        || $param->{'expire_on_date'} eq '0000-00-00' )
+    {
+        $param->{'expire_on_date'} = '';
+        $param->{'expire_on_time'} = '';
+    }
+    else {
+        $param->{'has_expire'} = 1;
     }
 }
 
 sub xfrm_preview_param {
-    my ($cb, $app, $param, $tmpl) = @_;
+    my ( $cb, $app, $param, $tmpl ) = @_;
 
     my $blog = $app->blog;
 
     my $date = $app->{query}->param('expire_on_date');
     my $time = $app->{query}->param('expire_on_time');
 
-    push @{$param->{entry_loop}}, {
-	data_name => 'expire_on_date',
-	data_value => $date,
-    };
-    push @{$param->{entry_loop}}, {
-	data_name => 'expire_on_time',
-	data_value => $time,
-    };
+    push @{ $param->{entry_loop} },
+      {
+        data_name  => 'expire_on_date',
+        data_value => $date,
+      };
+    push @{ $param->{entry_loop} },
+      {
+        data_name  => 'expire_on_time',
+        data_value => $time,
+      };
 }
 
 sub xfrm_list {
-    my ($cb, $app, $tmpl) = @_;
+    my ( $cb, $app, $tmpl ) = @_;
     my $slug;
     $slug = <<END_TMPL;
 <link rel="stylesheet" type="text/css" href="<mt:StaticWebPath>plugins/ExpiredEntries/app.css" />
@@ -100,12 +107,13 @@ END_TMPL
 }
 
 sub xfrm_table {
-    my ($cb, $app, $tmpl) = @_;
+    my ( $cb, $app, $tmpl ) = @_;
     my $slug;
     $slug = <<END_TMPL;
 <mt:if name="status" eq="6"> status-expired</mt:if>
 END_TMPL
-    $$tmpl =~ s{(<td class="status si<mt:if name="status_draft">.*?</mt:if>)}{$1 $slug}msgi;
+    $$tmpl =~
+s{(<td class="status si<mt:if name="status_draft">.*?</mt:if>)}{$1 $slug}msgi;
 
     $slug = <<END_TMPL;
             <mt:if name="status" eq="6">
@@ -116,19 +124,20 @@ END_TMPL
 }
 
 sub xfrm_list_param {
-    my ($cb, $app, $param, $tmpl) = @_;
+    my ( $cb, $app, $param, $tmpl ) = @_;
     my $table = $param->{'entry_table'}[0];
-    foreach my $e (@{ $table->{'object_loop'} }) {
-	if ($e->{status} == 6) {
-	    if ($e->{expire_on} < epoch2ts(time)) {
-		MT->log({ message => "Entry has expired on " . $e->{expire_on} });
-	    }
-	}
+    foreach my $e ( @{ $table->{'object_loop'} } ) {
+        if ( $e->{status} == 6 ) {
+            if ( $e->{expire_on} < epoch2ts(time) ) {
+                MT->log(
+                    { message => "Entry has expired on " . $e->{expire_on} } );
+            }
+        }
     }
 }
 
 sub xfrm_edit {
-    my ($cb, $app, $tmpl) = @_;
+    my ( $cb, $app, $tmpl ) = @_;
     my $slug;
     $slug = <<END_TMPL;
         <mt:setvarblock name="label"><mt:if name="status_expired">Expired On<mt:else>Expire On</mt:if></mt:setvarblock>
@@ -145,7 +154,8 @@ sub xfrm_edit {
             </span>
         </mtapp:setting>
 END_TMPL
-    $$tmpl =~ s{(<mtapp:setting\s+id="authored_on"[^>]*>.*?</mtapp:setting>)}{$1 $slug}msgi;
+    $$tmpl =~
+s{(<mtapp:setting\s+id="authored_on"[^>]*>.*?</mtapp:setting>)}{$1 $slug}msgi;
 
     $slug = <<END_TMPL;
                     <mt:if name="new_object">
@@ -156,7 +166,8 @@ END_TMPL
                         </mt:unless>
                     </mt:if>
 END_TMPL
-    $$tmpl =~ s{(<select name="status"[^>]*>.*?)</select>}{$1 $slug</select>}msgi;
+    $$tmpl =~
+      s{(<select name="status"[^>]*>.*?)</select>}{$1 $slug</select>}msgi;
 
     $slug = <<END_TMPL;
                         <mt:else name="status_expired">
@@ -166,66 +177,79 @@ END_TMPL
 }
 
 sub load_filters {
-    my $app = shift;
+    my $app  = shift;
     my $core = MT->component('Core');
 
-#    my $fltrs = $core->{registry}->{applications}->{cms}->{list_filters};
-#    delete $fltrs->{'entry'};
+    #    my $fltrs = $core->{registry}->{applications}->{cms}->{list_filters};
+    #    delete $fltrs->{'entry'};
 
     my $reg;
 
     $reg->{'entry'}->{'expired'} = {
-	label => sub {
-                    $app->translate( 'Expired [_1]', _entry_label );
-                },
-	order => 100,
-	handler => sub {
-	    my ( $terms, $args ) = @_;
-	    $terms->{status} = MT::Entry::EXPIRED();
-	},
+        label => sub {
+            $app->translate( 'Expired [_1]', _entry_label );
+        },
+        order   => 100,
+        handler => sub {
+            my ( $terms, $args ) = @_;
+            $terms->{status} = MT::Entry::EXPIRED();
+        },
     };
     return $reg;
 }
 
 sub pre_save {
-    my ($cb, $app, $obj, $orig) = @_;
+    my ( $cb, $app, $obj, $orig ) = @_;
 
-    my $date = trim($app->param('expire_on_date'));
-    my $time = trim($app->param('expire_on_time'));
+    my $date = trim( $app->param('expire_on_date') );
+    my $time = trim( $app->param('expire_on_time') );
 
-    if (!$date || !$time || $date eq '' || $time eq '') {
-	$obj->expire_on( undef );
-	return 1;
+    if ( !$date || !$time || $date eq '' || $time eq '' ) {
+        $obj->expire_on(undef);
+        return 1;
     }
 
     my $error;
-    if ($date eq '0000-00-00') {
-	return $app->error( $app->translate(
-	    "Invalid date '[_1]'; The date you provided to not appear to be a real date.",
-	    $date
-			    ) );
+    if ( $date eq '0000-00-00' ) {
+        return $app->error(
+            $app->translate(
+"Invalid date '[_1]'; The date you provided to not appear to be a real date.",
+                $date
+            )
+        );
     }
 
-    my $eod  = $date ." ". $time;
+    my $eod = $date . " " . $time;
 
-    unless ( $eod =~ m!^(\d{4})-(\d{1,2})-(\d{1,2})\s+(\d{1,2}):(\d{1,2})(?::(\d{1,2}))?$! ) {
-	return $app->error( $app->translate(
-				"Invalid date '[_1]'; authored on dates must be in the format YYYY-MM-DD HH:MM:SS.",
-				$eod
-			    ) );
+    unless ( $eod =~
+        m!^(\d{4})-(\d{1,2})-(\d{1,2})\s+(\d{1,2}):(\d{1,2})(?::(\d{1,2}))?$! )
+    {
+        return $app->error(
+            $app->translate(
+"Invalid date '[_1]'; authored on dates must be in the format YYYY-MM-DD HH:MM:SS.",
+                $eod
+            )
+        );
     }
 
     my $s = $6 || 0;
-    if ($s > 59	|| $s < 0 || 
-	$5 > 59 || $5 < 0 || 
-	$4 > 23	|| $4 < 0 || 
-	$2 > 12	|| $2 < 1 || 
-	$3 < 1  || 
-	( days_in( $2, $1 ) < $3 && !MT::Util::leap_day( $0, $1, $2 ) ) ) {
-	return $app->error( $app->translate(
-	    "Invalid date '[_1]'; expired on dates should be real dates.",
-	    $eod
-			    ) );
+    if (   $s > 59
+        || $s < 0
+        || $5 > 59
+        || $5 < 0
+        || $4 > 23
+        || $4 < 0
+        || $2 > 12
+        || $2 < 1
+        || $3 < 1
+        || ( days_in( $2, $1 ) < $3 && !MT::Util::leap_day( $0, $1, $2 ) ) )
+    {
+        return $app->error(
+            $app->translate(
+                "Invalid date '[_1]'; expired on dates should be real dates.",
+                $eod
+            )
+        );
     }
 
     my $ts = sprintf "%04d%02d%02d%02d%02d%02d", $1, $2, $3, $4, $5, $s;
@@ -239,32 +263,42 @@ sub task_expire {
     require MT::Util;
     my $mt            = MT->instance;
     my $total_changed = 0;
-    my @blogs = MT->model('blog')->load(undef, {
-        join => MT->model('entry')->join_on('blog_id', {
-            status => MT::Entry::RELEASE(),
-            expire_on => { not_null => 1 },
-	   }, { unique => 1 })
-    });
+    my @blogs         = MT->model('blog')->load(
+        undef,
+        {
+            join => MT->model('entry')->join_on(
+                'blog_id',
+                {
+                    status    => MT::Entry::RELEASE(),
+                    expire_on => { not_null => 1 },
+                },
+                { unique => 1 }
+            )
+        }
+    );
     foreach my $blog (@blogs) {
         my @ts = offset_time_list( time, $blog );
         my $now = sprintf "%04d%02d%02d%02d%02d%02d", $ts[5] + 1900, $ts[4] + 1,
-	@ts[ 3, 2, 1, 0 ];
+          @ts[ 3, 2, 1, 0 ];
         my $iter = MT->model('entry')->load_iter(
             {
-                blog_id => $blog->id,
-                status  => MT::Entry::RELEASE(),
-		expire_on => [ undef, $now ],
-                class   => '*'
+                blog_id   => $blog->id,
+                status    => MT::Entry::RELEASE(),
+                expire_on => [ undef, $now ],
+                class     => '*'
             },
             {
-		'range'     => { 'expire_on' => 1 },
+                'range'     => { 'expire_on' => 1 },
                 'direction' => 'descend'
             }
-	    );
+        );
         my @queue;
         while ( my $entry = $iter->() ) {
-	    next unless $entry->expire_on && $entry->expire_on ne '' && $entry->expire_on ne '00000000000000';
-	    push @queue, $entry->id if $entry->expire_on <= $now;
+            next
+              unless $entry->expire_on
+                  && $entry->expire_on ne ''
+                  && $entry->expire_on ne '00000000000000';
+            push @queue, $entry->id if $entry->expire_on <= $now;
         }
 
         my $changed = 0;
@@ -272,11 +306,20 @@ sub task_expire {
         my %rebuild_queue;
         foreach my $entry_id (@queue) {
             my $entry = MT->model('entry')->load($entry_id)
-                or next;
+              or next;
             $entry->status( MT::Entry::EXPIRED() );
-	    MT->log({ blog_id => $entry->blog_id, message => "Expiring " . $entry->class . " " . $entry->title . " with expiration date of " . $entry->expire_on });
+            MT->log(
+                {
+                    blog_id => $entry->blog_id,
+                    message => "Expiring "
+                      . $entry->class . " "
+                      . $entry->title
+                      . " with expiration date of "
+                      . $entry->expire_on
+                }
+            );
             $entry->save
-		or die $entry->errstr;
+              or die $entry->errstr;
 
             MT->run_callbacks( 'post_expired', $mt, $entry );
 
@@ -296,14 +339,15 @@ sub task_expire {
                 {
                     my $entry = $rebuild_queue{$id};
                     $mt->rebuild_entry( Entry => $entry, Blog => $blog )
-			or die $mt->errstr;
+                      or die $mt->errstr;
                     $rebuilt_okay{$id} = 1;
                     $rebuilt++;
                 }
                 $mt->rebuild_indexes( Blog => $blog )
-		    or die $mt->errstr;
+                  or die $mt->errstr;
             };
             if ( my $err = $@ ) {
+
                 # a fatal error occured while processing the rebuild
                 # step. LOG the error and revert the entry/entries:
                 require MT::Log;
@@ -317,7 +361,7 @@ sub task_expire {
                         blog_id => $blog->id,
                         level   => MT::Log::ERROR()
                     }
-		    );
+                );
                 foreach my $id (@queue) {
                     next if exists $rebuilt_okay{$id};
                     my $e = $rebuild_queue{$id};
